4624c4c71ab4a50fc6dfe07855eb1e2a785c720a
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-12-02 14:15:58 -0500

    solve y24d1p2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use advent_of_code::request_input;
 use advent_of_code::result::Result;
+use std::fs;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let inputs_dir = Path::new("inputs");
+    if !inputs_dir.exists() {
+        fs::create_dir(inputs_dir)?;
+        fs::write("inputs/.gitignore", "*")?;
+    }
+
     for day in 1..=25 {
         request_input(day).await?;
     }
--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -1,10 +1,9 @@
 use crate::result::Result;
 use regex::Regex;
 
-fn part1(lines: Vec<String>) -> Result<i32> {
-    let mut diffs = Vec::new();
-    let mut left = Vec::new();
-    let mut right = Vec::new();
+fn get_left_right(lines: Vec<String>) -> Result<(Vec<i32>, Vec<i32>)> {
+    let mut left = vec![];
+    let mut right = vec![];
 
     let re = Regex::new(r"^(?<left>\d+) +(?<right>\d+)$")?;
     for line in lines {
@@ -13,8 +12,16 @@ fn part1(lines: Vec<String>) -> Result<i32> {
         right.push(*&caps["right"].parse::<i32>()?);
     }
 
+    Ok((left, right))
+}
+
+fn part1(lines: Vec<String>) -> Result<i32> {
+    let (mut left, mut right) = get_left_right(lines.clone())?;
+
     left.sort();
     right.sort();
+
+    let mut diffs = vec![];
 
     for (l, r) in left.iter().zip(right.iter()) {
         let diff = l - r;
@@ -26,6 +33,20 @@ fn part1(lines: Vec<String>) -> Result<i32> {
     }
 
     Ok(diffs.iter().sum())
+}
+
+fn part2(lines: Vec<String>) -> Result<i32> {
+    let (mut left, mut right) = get_left_right(lines.clone())?;
+
+    let mut prods = vec![];
+
+    for l in left.iter() {
+        let count = right.iter().filter(|&r| l == r).count();
+        let prod = l * count as i32;
+        prods.push(prod);
+    }
+
+    Ok(prods.iter().sum())
 }
 
 #[cfg(test)]
@@ -45,6 +66,27 @@ mod tests {
         ];
         let example = part1(lines).unwrap();
         assert_eq!(example, 11);
-        println!("{}", part1(get_lines(1).unwrap()).unwrap());
+
+        let lines = get_lines(1).unwrap();
+        let answer = part1(lines).unwrap();
+        println!("{}", answer);
+    }
+
+    #[test]
+    fn test_part2() {
+        let lines = vec![
+            "3   4".to_string(),
+            "4   3".to_string(),
+            "2   5".to_string(),
+            "1   3".to_string(),
+            "3   9".to_string(),
+            "3   3".to_string(),
+        ];
+        let example = part2(lines).unwrap();
+        assert_eq!(example, 31);
+
+        let lines = get_lines(1).unwrap();
+        let answer = part2(lines).unwrap();
+        println!("{}", answer);
     }
 }
